aa5deba88aca4d9762c2e99678336b6a0476cb02
agussamper <agus03z@hotmail.com>
2024-02-08 20:08:53 -0300

    fix:arr_func

--- src/arr_func.c
+++ src/arr_func.c
@@ -5,7 +5,7 @@
 uint32_t arrcmp(char* arr1,
   uint32_t len1,
   char* arr2, uint32_t len2){
-  if (len1 != len2) return 0;
+  if (len1 != len2) return 1;
   for(uint32_t i = 0; i < len1; i++){
     if(arr1[i] != arr2[i]) return 1;
   }
